bf6e326faba2a85fbfc7a0a4c3a10c21f78fee8a
Marc Cavigli <mcavigli@gmail.com>
2019-09-09 21:53:10 +0000

    Added stats and cleaned up directory

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,29 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'])
 def json_ret():
     """ returns a JSON string on an object """
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def obj_num():
+    """ retrieves number of objects by type """
+    objs = {
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User')
+    }
+    return jsonify(objs)
