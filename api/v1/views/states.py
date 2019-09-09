8776ecb0c569cc7059de9efde611d42f74c95659
Tu Vo <tuvo1106@gmail.com>
2019-09-09 13:20:27 +0000

    Refactor and add cascade-delete to states/cities

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,62 +1,66 @@
 #!/usr/bin/python3
+
+"""
+Create a new view for State objects that handles
+all default RestFul API actions.
+"""
+
 from api.v1.views import app_views
-from flask import Flask, jsonify, request, abort, Response
+from flask import jsonify, request, abort
 from models import storage
 from models.state import State
 
 
 @app_views.route('/states', strict_slashes=False, methods=['GET', 'POST'])
-@app_views.route('/states/<state_id>', strict_slashes=False, methods=['GET',
-                                                                      'DELETE',
-                                                                      'PUT'])
+@app_views.route('/states/<state_id>',
+                 strict_slashes=False,
+                 methods=['GET', 'DELETE', 'PUT'])
 def get_state(state_id=None):
-    """  """
-    objs = storage.all("State").values()
+    """Returns GET, DELETE, PUT, POST methods"""
     if request.method == 'GET':
-        obj = []
-        if state_id == None:
-            for i in objs:
-                obj.append(i.to_dict())
-            return jsonify(obj)
+        if state_id is None:
+            objs = storage.all("State").values()
+            _list = []
+            for obj in objs:
+                _list.append(obj.to_dict())
+            return jsonify(_list), 200
         else:
-            for i in objs:
-                if i.id == state_id:
-                    return jsonify(i.to_dict())
+            state = storage.get("State", state_id)
+            if state:
+                return jsonify(state.to_dict()), 200
             abort(404)
     elif request.method == 'DELETE':
-        for i in objs:
-            if i.id == state_id:
-                print("***")
-                print(i)
-                print("***")
-                storage.delete(i)
-                #i.delete()
-                storage.save()
-                return jsonify({}), 200
-        abort(404)
+        state = storage.get("State", state_id)
+        if state:
+            storage.delete(state)
+            storage.save()
+            storage.reload()
+            return jsonify({}), 200
+        else:
+            abort(404)
     elif request.method == 'POST':
         if not request.content_type == 'application/json':
             return jsonify({'error': 'Not a JSON'}), 400
         req = request.get_json()
-        # req == {name: ca}
         if 'name' not in req:
             return jsonify({'error': 'Missing name'}), 400
         new_state = State(**req)
         new_state.save()
         objs = storage.all("State").values()
-        for i in objs:
-            if i.name == req['name']:
-                return jsonify(i.to_dict()), 201
+        for obj in objs:
+            if obj.name == req['name']:
+                return jsonify(obj.to_dict()), 201
     elif request.method == "PUT":
         if not request.content_type == 'application/json':
             return jsonify({'error': 'Not a JSON'}), 400
         req = request.get_json()
         ignore = ['created_at', 'updated_at', 'id']
-        for i in objs:
-            if i.id == state_id:
-                for k, v in req.items():
-                    if k not in ignore:
-                        i.k = v
-                        storage.save()
-                        return jsonify(i.to_dict()), 200
+        state = storage.get("State", state_id)
+        if state:
+            for k, v in req.items():
+                if k not in ignore:
+                    setattr(state, k, v)
+                    storage.save()
+                    storage.reload()
+                    return jsonify(state.to_dict()), 200
         abort(404)
