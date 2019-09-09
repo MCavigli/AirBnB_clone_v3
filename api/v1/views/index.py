c9b8914d47395116ca04bcf722209ca98fee76bf
Marc Cavigli <mcavigli@gmail.com>
2019-09-09 21:21:02 +0000

    Removing directory from this branch

--- api/v1/views/index.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-from api.v1.views import app_views
-from flask import Flask, jsonify
-
-
-@app_views.route('/status', methods=['GET'])
-def json_ret():
-    """ returns a JSON string on an object """
-    return jsonify({"status": "OK"})
