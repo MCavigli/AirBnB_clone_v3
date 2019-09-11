6c1edefd60890217f8da5696db60e677676708c8
Marc Cavigli <mcavigli@gmail.com>
2019-09-11 19:50:02 +0000

    Added CORS functionality

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@ Entry point for flask
 """
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -12,6 +13,7 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
+cors = CORS(app, resources={"*": {"origins": "0.0.0.0"}})
 
 ip = getenv("HBNB_API_HOST") or '0.0.0.0'
 port = getenv("HBNB_API_PORT") or 5000
